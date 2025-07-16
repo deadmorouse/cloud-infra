c1b17b3621f2e582ae9dbb7efd434764665b8050
Suntsov_Igor <iksok2016@mail.ru>
2025-07-16 11:42:50 +0300

    changesome

--- 2hw/bucket.tf
+++ 2hw/bucket.tf
@@ -16,13 +16,11 @@ resource "yandex_resourcemanager_folder_iam_member" "bucket-editor" {
     depends_on = [yandex_iam_service_account.netology]
 }
 
-// Create Static Access Keys
 resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
     service_account_id = yandex_iam_service_account.netology.id
     description        = "static access key for bucket"
 }
 
-// Use keys to create bucket
 resource "yandex_storage_bucket" "netology-bucket" {
     access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
     secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
